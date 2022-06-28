2f05bcd8f1516751f85b47014f30c36378db4052
foxxxesky <abdurrahman1467@gmail.com>
2022-06-28 12:21:08 +0800

    add bootstrap

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,11 @@
 <template>
-  <h1 class="text-center">WellFood</h1>
+  <div>
+    <Nuxt />
+  </div>
 </template>
 
 <script>
 export default {
-  name: "IndexPage",
+  name: "Main-Page",
 };
 </script>
